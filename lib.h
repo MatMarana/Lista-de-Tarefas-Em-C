3bff9f5d3d04fd292dfeb0118d7f4905a9a41e54
Mateus Marana <assumateus@gmail.com>
2023-11-12 22:35:06 -0300

    feature[Alterar Tarefas]

--- lib.h
+++ lib.h
@@ -3,6 +3,7 @@
 
 typedef struct Tarefas { 
  int prioridade;
+ char estado[15];
  char categoria[100];
  char descricao[300];
 } Tarefas;
@@ -11,4 +12,9 @@ void criaTarefas(int numeroTarefa);
 void listaTarefas(int numeroTarefa);
 void deltarTarefa(int numeroTarefa, int prioridade);
 void verficaPrioridade(int numeroDeTarefa);
+void alteraTarefa(int numeroDeTarefa, int prioridade);
+void filtraPrioridade(int numeroDeTarefa, int prioridade);
+void filtraEstado(int numeroDeTarefa, char estado[15]);
+void filtraCategoria(int numeroDeTarefa, char categoria[100]);
+void filtraPrioridadeCategoria(int numeroDeTarefa, int prioridade, char categroia[100]);
 #endif 
